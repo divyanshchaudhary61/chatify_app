4c57230a8ab2b1fa34643021294f557d81bc13e8
divyanshchaudhary61 <divyanshchaudhary6161@gmail.com>
2025-05-24 23:23:59 +0530

    Changes done

--- front-end/src/context/SocketContext.js
+++ front-end/src/context/SocketContext.js
@@ -13,7 +13,7 @@ export const SocketContextProvider = ({ children }) => {
     useEffect(() => {
 
         if (authUser) {
-            const socket = io("http://localhost:8080",{
+            const socket = io("https://chatify-backend-service.onrender.com",{
                 query: {
                     userId: authUser._id,
                 }
